8944bc70f1adf40ac3f6fc6e073227601093b888
rohit yadav <rohityadavcsky@gmail.com>
2024-09-29 22:00:43 +0530

    progress project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import './App.css'
+import './App.css'
 import Main from './Component/Main'
 function App() {
   return (
--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,4 +1,43 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+
 const Main = () =>{
+
+    const [subject, setSubject] = useState('');
+    const [hours, setHours] = useState('');
+
+    const onClickButton = () => {
+        alert("Please check!!");
+        console.log("Subject: " + subject + ", Hours: " + hours);
+    }
+
+    useEffect(()=>{
+        console.log("Subject: " + subject + ", Hours: " + hours);
+    },[subject, hours]);
+    
+    const SubjectInput = (e) => {
+        setSubject(e.target.value);
+    }
+    
+    const HoursInput = (e) => {
+        setHours(e.target.value);
+    }
+    
+    const AddButton = () => {
+        onClickButton();
+        setSubject('');
+        setHours('');
+    }
+    
+
+    const ResetButton = () =>{
+        setSubject('');
+        setHours('');
+        alert("Reset Successfully!!");
+        console.log("Reset Successful!!");
+    }
+    
     return (
         <>
         <div className="container">
@@ -6,14 +45,20 @@ const Main = () =>{
         <h3>Geekster Education Planner</h3>
         </div>
         <div className="sub">
-
-        <input type="text" name="name" placeholder="Subject"/>
-        </div>
-        <div className="num">
-        <input type="number" name="number"/>
+        <input type="text" name="name" placeholder="Subject" id="subject"/>
+        <input type="number" name="number" placeholder="Hou"/>
         </div>
         <div className="btn">
-        <button>Add</button>
+        <button onClick={onClickButton} >Add</button>
+        <button onClick={ResetButton}>RESET</button>
+        </div>
+
+        <div>
+            <h3>Added Subjects:</h3>
+            <div>
+                {subject? <p>{subject} - {hours} Hours</p> : <p>No Subjects added yet</p>}
+            </div>
+           
         </div>
 
         </div>
